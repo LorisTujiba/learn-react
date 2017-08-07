36a47b2dd35dcac35863ea5fc49bf1c727b04157
Loris Tujiba <pisceslrz@gmail.com>
2017-08-08 00:00:51 +0700

    created goal-coach

---
+++ goal-coach/src/components/SignUp.jsx
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+import {firebaseApp} from '../firebase';
+
+class SignUp extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      email :'',
+      password:'',
+      err:{
+        message:''
+      }
+    }
+  }
+
+  signUp(){
+    console.log(this.state);
+    const {email,password} = this.state;
+    firebaseApp.auth().createUserWithEmailAndPassword(email,password)
+      .catch(err => {
+        this.setState({err})
+      })
+  }
+
+  render(){
+    return(
+      <div className="form-inline">
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <input
+            className="form-control"
+            placeholder="email"
+            onChange={event => this.setState({email: event.target.value})}
+          />
+          <input
+            className="form-control"
+            type="password"
+            placeholder="password"
+            onChange={event => this.setState({password: event.target.value})}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={()=>this.signUp()}
+          >
+            Sign Up
+          </button>
+        </div>
+        <div>{this.state.err.message}</div>
+      </div>
+    )
+  }
+}
+
+export default SignUp;
---
+++ goal-coach/src/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCoy9UWZ87G-7Ig7FJYP4GfN54VqClDu_o",
+    authDomain: "goal-coach-f6917.firebaseapp.com",
+    databaseURL: "https://goal-coach-f6917.firebaseio.com",
+    projectId: "goal-coach-f6917",
+    storageBucket: "",
+    messagingSenderId: "234173359517"
+};
+export const firebaseApp = firebase.initializeApp(config);
